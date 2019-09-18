3e7c890ea84d8473903418dbe7e3e328ef2a51b0
ElenaVeselovskaya <611717Elena>
2019-09-18 18:16:38 +0300

    Update 'Main' #1 fixes #3 Added '_' to separate digits fixes #4 Rendered the digits in variables fixes #5 Deleted the 2nd condition in 'if' statement

--- src/Main.java
+++ src/Main.java
@@ -22,12 +22,10 @@ public class Main {
             System.out.println("The total amount of purchases is more 30_000 rub. The sum of current purchase with 20% discount is " + (currentSumOfPurchaseInRub - totalSumWithTwentyPercentDiscountInRub) + " rub.");
             System.out.println("The sum of 20% discount is " + totalSumWithTwentyPercentDiscountInRub + " rub.");
         } else {
-
             if (totalAmountOfPurchasesInRub >= limitOfTenPercentDiscountInRub) {
                 System.out.println("The total amount of purchases is more 15_000 rub and less 30_000 rub. The sum of current purchase with 10% discount is " + (currentSumOfPurchaseInRub - totalSumWithTenPercentDiscountInRub) + " rub.");
                 System.out.println("The sum of 10% discount is " + totalSumWithTenPercentDiscountInRub + " rub.");
             } else {
-
                 if (totalAmountOfPurchasesInRub >= limitOfFivePercentDiscountInRub) {
                     System.out.println("The total amount of purchases is more 7_500 rub and less 15_000 rub. The sum of current purchase with 5% discount is " + (currentSumOfPurchaseInRub - totalSumWithFivePercentDiscountInRub) + " rub.");
                     System.out.println("The sum of 5% discount is " + totalSumWithFivePercentDiscountInRub + " rub.");
