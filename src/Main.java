e7b0d22a738c0dfa53d94193d9a901593b7bd880
ElenaVeselovskaya <611717Elena>
2019-09-20 12:21:35 +0300

    Update 'Main' #1 fixes #2 Rename repository fixes #6 Formatted the code

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
 
         int limitOfFivePercentDiscountInRub = 7_500;
         int limitOfTenPercentDiscountInRub = 15_000;
-        int limitOfTwentuPercentDiscountInRub = 30_000;
+        int limitOfTwentyPercentDiscountInRub = 30_000;
 
-        int totalAmountOfPurchasesInRub = 30_000;
+        int totalAmountOfPurchasesInRub = 7000;
         int currentSumOfPurchaseInRub = 10_000;
 
         int hundredPercent = 100;
@@ -18,21 +18,17 @@ public class Main {
         int totalSumWithTenPercentDiscountInRub = currentSumOfPurchaseInRub * tenPercentDiscount / hundredPercent;
         int totalSumWithTwentyPercentDiscountInRub = currentSumOfPurchaseInRub * twentyPercentDiscount / hundredPercent;
 
-        if (totalAmountOfPurchasesInRub >= limitOfTwentuPercentDiscountInRub) {
+        if (totalAmountOfPurchasesInRub >= limitOfTwentyPercentDiscountInRub) {
             System.out.println("The total amount of purchases is more 30_000 rub. The sum of current purchase with 20% discount is " + (currentSumOfPurchaseInRub - totalSumWithTwentyPercentDiscountInRub) + " rub.");
             System.out.println("The sum of 20% discount is " + totalSumWithTwentyPercentDiscountInRub + " rub.");
+        } else if (totalAmountOfPurchasesInRub >= limitOfTenPercentDiscountInRub) {
+            System.out.println("The total amount of purchases is more 15_000 rub and less 30_000 rub. The sum of current purchase with 10% discount is " + (currentSumOfPurchaseInRub - totalSumWithTenPercentDiscountInRub) + " rub.");
+            System.out.println("The sum of 10% discount is " + totalSumWithTenPercentDiscountInRub + " rub.");
+        } else if (totalAmountOfPurchasesInRub >= limitOfFivePercentDiscountInRub) {
+            System.out.println("The total amount of purchases is more 7_500 rub and less 15_000 rub. The sum of current purchase with 5% discount is " + (currentSumOfPurchaseInRub - totalSumWithFivePercentDiscountInRub) + " rub.");
+            System.out.println("The sum of 5% discount is " + totalSumWithFivePercentDiscountInRub + " rub.");
         } else {
-            if (totalAmountOfPurchasesInRub >= limitOfTenPercentDiscountInRub) {
-                System.out.println("The total amount of purchases is more 15_000 rub and less 30_000 rub. The sum of current purchase with 10% discount is " + (currentSumOfPurchaseInRub - totalSumWithTenPercentDiscountInRub) + " rub.");
-                System.out.println("The sum of 10% discount is " + totalSumWithTenPercentDiscountInRub + " rub.");
-            } else {
-                if (totalAmountOfPurchasesInRub >= limitOfFivePercentDiscountInRub) {
-                    System.out.println("The total amount of purchases is more 7_500 rub and less 15_000 rub. The sum of current purchase with 5% discount is " + (currentSumOfPurchaseInRub - totalSumWithFivePercentDiscountInRub) + " rub.");
-                    System.out.println("The sum of 5% discount is " + totalSumWithFivePercentDiscountInRub + " rub.");
-                } else {
-                    System.out.println("The total amount of purchases is less 7500 rub. The sum without discount is " + currentSumOfPurchaseInRub + " rub.");
-                }
-            }
+            System.out.println("The total amount of purchases is less 7500 rub. The sum without discount is " + currentSumOfPurchaseInRub + " rub.");
         }
     }
 }
